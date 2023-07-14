59ab0c55026099a30d62cf514dbc53d1069892df
danny-423 <122214622+danny-423@users.noreply.github.com>
2023-07-14 22:00:27 +0530

    Update ml10.py

--- ml10.py
+++ ml10.py
@@ -9,30 +9,18 @@ Y = datasets.iloc[:, 4].values
 from sklearn.model_selection import train_test_split
 X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
-
-
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_Train = sc_X.fit_transform(X_Train)
 X_Test = sc_X.transform(X_Test)
 
-
-
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_Train, Y_Train)
-
-
 Y_Pred = classifier.predict(X_Test)
-
-
 
 from sklearn import metrics
 print("Accuracy score", metrics.accuracy_score(Y_Test, Y_Pred))
-
-
-
-
 
 plt.scatter(X_Train[:,0],X_Train[:, 1], c=Y_Train)
 
@@ -46,10 +34,6 @@ xx = np.linspace(-2.5,2.5)
 yy = a*xx - (classifier.intercept_[0])/w[1]
 plt.plot(xx,yy)
 plt.show()
-
-
-
-
 plt.scatter(X_Test[:,0],X_Test[:, 1],c=Y_Test)
 plt.title('Support Vector Machine (Test set)')
 plt.xlabel('Age')
